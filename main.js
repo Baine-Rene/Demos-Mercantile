09b9f43ea66ddca684451be2cd7d08db95c91ffb
Rene Baine <renebaine13@gmail.com>
2024-01-31 13:59:07 +0300

    go-to-top Update

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
         $('.back-to-top').click(function () {
-            $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+            $('html, body').animate({scrollTop: 0}, 1000, 'easeInOutExpo');
             return false;
         });
 
